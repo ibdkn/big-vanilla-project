8d49d2d3d016ad8a88858936f178caf42b9b8de5
ibdkn <ibudiakina@gmail.com>
2024-03-30 10:05:32 +0300

    wrote simple tests for governmentBuildings

--- src/02-objects/02.test.tsx
+++ src/02-objects/02.test.tsx
@@ -5,8 +5,60 @@ let city: CityType;
 beforeEach(() => {
     city = {
         title: "New York",
-        houses: [],
-        governmentBuildings: [],
+        houses: [
+            {
+                buildedAt: 2012,
+                repaired: false,
+                address: {
+                    number: 100,
+                    street: {
+                        title: "White street"
+                    }
+                }
+            },
+            {
+                buildedAt: 2008,
+                repaired: false,
+                address: {
+                    number: 100,
+                    street: {
+                        title: "Happy street"
+                    }
+                }
+            },
+            {
+                buildedAt: 2020,
+                repaired: false,
+                address: {
+                    number: 200,
+                    street: {
+                        title: "Hogwarts street"
+                    }
+                }
+            },
+        ],
+        governmentBuildings: [
+            {
+                type: "HOSPITAL",
+                budget: 200000,
+                staffCount: 200,
+                address: {
+                    street: {
+                        title: "Central Str"
+                    }
+                }
+            },
+            {
+                type: "FIRE-STATION",
+                budget: 500000,
+                staffCount: 1000,
+                address: {
+                    street: {
+                        title: "Souths park str"
+                    }
+                }
+            },
+        ],
         citizensNumber: 1000000
     }
 })
@@ -30,7 +82,7 @@ test("city should contains 3 houses", () => {
     expect(city.houses[2].address.street.title).toBe("Hogwarts street");
 })
 
-test.skip("city should contains hospital and fire station", () => {
+test("city should contains hospital and fire station", () => {
     expect(city.governmentBuildings.length).toBe(2);
 
     expect(city.governmentBuildings[0].type).toBe("HOSPITAL")
--- src/02-objects/02_02.ts
+++ src/02-objects/02_02.ts
@@ -1,6 +1,29 @@
 export type CityType = {
     title: string,
-    houses: Array<number>,
-    governmentBuildings: Array<string>,
+    houses: Array<HouseType>,
+    governmentBuildings: Array<GovernmentBuildingType>,
     citizensNumber: number
 }
+
+export type HouseType = {
+    buildedAt: number
+    repaired: boolean
+    address: AddressType
+}
+
+export type AddressType = {
+    number?: number
+    street: StreetType
+}
+
+export type StreetType = {
+    title: string
+}
+
+export type GovernmentBuildingType = {
+    type: "HOSPITAL" | "FIRE-STATION"
+    budget: number
+    staffCount: number
+    address: AddressType
+}
+
